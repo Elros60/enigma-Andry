d8e857789789187880259dc38a0fc2a4b24baae0
ChiZHANG <chi.zhang@cern.ch>
2023-07-14 18:42:31 +0200

    multi-task submitting

--- grid/runReco.sh
+++ grid/runReco.sh
@@ -3,7 +3,7 @@ CTF_LIST=`alien_ls $1`
 
 for CTF in $CTF_LIST
 do
-	CMD="alien_submit /alice/cern.ch/user/c/chizh/mch/commissioning/2023/reco.jdl 535047 LHC23e ${CTF:0:4} 10800"
+	CMD="alien_submit /alice/cern.ch/user/c/chizh/mch/commissioning/2023/reco.jdl 535047 LHC23e ${CTF:0:4} 7200"
 	echo $CMD
 	${CMD}
 done
