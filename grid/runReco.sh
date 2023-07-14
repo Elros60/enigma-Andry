f79423da23e5a1227bf503a60b4a1cdbbd5f64a5
ChiZHANG <chi.zhang@cern.ch>
2023-07-14 15:51:30 +0200

    updates and new scripts for submit mulitple tasks on Grid

---
+++ grid/runReco.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CTF_LIST=`alien_ls $1`
+
+for CTF in $CTF_LIST
+do
+	CMD="alien_submit /alice/cern.ch/user/c/chizh/mch/commissioning/2023/reco.jdl 535047 LHC23e ${CTF:0:4} 10800"
+	echo $CMD
+	${CMD}
+done
