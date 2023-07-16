4faaab85a27dc90eb32d74b48c09c4bbb5191851
ChiZHANG <chi.zhang@cern.ch>
2023-07-17 00:38:48 +0200

    updates

--- grid/run_ctfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CTF_LIST=`alien_ls $TARGET_DIR`
-
-for CTF in $CTF_LIST
-do
-	CMD="submit /alice/cern.ch/user/c/chizh/mch/commissioning/2023/reco.jdl 535047 LHC23e ${CTF:0:4} 10000"
-	echo $CMD
-	${CMD}
-done
